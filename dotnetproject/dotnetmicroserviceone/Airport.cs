e7a68f0b3679f49d5094093eebab7b552d0a439a
Iamneo <techadmin@iamneo.ai>
2023-10-04 10:46:12 +0000

    Backup Commit

---
+++ dotnetproject/dotnetmicroserviceone/Airport.cs
@@ -0,0 +1,13 @@
+namespace dotnetmicroserviceone;
+
+    public class Airport
+    {
+        public int AirportID {get;set;}
+        public string AirportName {get;set;}
+        public string Location {get;set;}
+        public string Country {get;set;}
+        public string Website {get;set;}
+        public string ContactNumber {get;set;}
+        public string Email {get;set;}
+        public string Description {get;set;}
+    }
