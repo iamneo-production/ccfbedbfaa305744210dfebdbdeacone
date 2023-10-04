2b1f561d0939f46c060fdfefc7c06d1808ee6d3f
Iamneo <techadmin@iamneo.ai>
2023-10-04 10:53:21 +0000

    Backup Commit

--- dotnetproject/dotnetmicroserviceone/Models/AirportDbContext.cs
+++ dotnetproject/dotnetmicroserviceone/Models/AirportDbContext.cs
@@ -8,6 +8,8 @@ namespace dotnetmicroserviceone.Models
 {
     public class AirportDbContext : DbContext
     {
-        public AirportDbContext(DbContextOptions<) : base(options)
+        public AirportDbContext(DbContextOptions<AirportDbContext> options) : base(options)
+        {}
+        public DbSet<Airport> AirportItems {get;set;} = null;
     }
 }
